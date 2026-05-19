8b9973ca735409c1f398d97de7c21687cee0afc9
andre-nemesis <andrecasimio@gmail.com>
2026-05-19 13:23:52 -0300

    fix & deploy: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -33,4 +33,10 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+
+  base: '/portifolio/',                    
+  build: {
+    outDir: 'dist',             
+    assetsDir: 'assets',        
+  },
 })
